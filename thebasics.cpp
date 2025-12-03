a44163bbff29bdc99635ffd8e276b492b668cbc4
jpcoder12 <jpcoder12@gmail.com>
2025-12-02 21:39:20 -0700

    Ordinal classification of day of year

--- thebasics.cpp
+++ thebasics.cpp
@@ -5,6 +5,14 @@ using namespace std; /* why use many word when few word do trick? */
 #include <iomanip> // for manipulating outputs (decimals)
 #include <string>
 #include <limits>
+
+int define_ordinal(int dayOfYr) {
+    string rt_val = to_string(dayOfYr);
+    // I want the last digit, which will always be 2 in index
+    rt_val = rt_val[2];
+    cout << rt_val << " << rt val\n";
+    return stoi(rt_val);
+}
 int main() {
     cout << "Hello World!\n"; // output to console
     cout << "Is Josh Okay?...Why don't you ask him? ";
@@ -18,12 +26,27 @@ int main() {
 
     // Converting the time into local time structure using tm
     tm tm_struct = *std::localtime(&t_c);
-    double dayOfYear = tm_struct.tm_yday, yearPercentage = (dayOfYear/365.0)*100;
+    int dayOfYear = tm_struct.tm_yday;
+    double yearPercentage = ((double)dayOfYear/365.0)*100;
     const int year = tm_struct.tm_year + 1900;
     
     int userAge;
     bool validInput = false;
-    cout << "It is the " << dayOfYear << "th day of the year" << endl;
+    string cronString;
+    int ordinal = define_ordinal(dayOfYear);
+    // change the ordinal nomenclature appended to day number depending on the ending value
+    if (ordinal > 3 || ordinal == 0){
+        cronString = "th";
+    } else if (ordinal == 1) {
+        cronString = "st";
+    } else if (ordinal == 2) {
+        cronString = "nd";
+    } else if (ordinal == 3) {
+        cronString = "rd";
+    }
+
+    // define_ordinal(dayOfYear);
+    cout << "It is the " << dayOfYear << cronString << " day of the year" << endl;
     cout << "We are " << fixed << setprecision(0)<< yearPercentage << "% through " << year <<"!" << endl;
     cout << "But the way...how old are you? \n";
     
@@ -69,7 +92,6 @@ int main() {
     if (userName.length() > 1) {
         cout << "Your name is " << userName.length() << " characters long. Interesting." << endl;
         cout << "The second letter of your name is " << userName[1] << endl;
-        
     }
     
 
